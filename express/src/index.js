5df845193eb2f708d73b2462bf91665c3cd048d6
Cássio Marcião <marciaocassio@gmail.com>
2025-03-12 18:01:28 -0300

    feat: Add express api

---
+++ express/src/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (_req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Server is running at http://localhost:3000. 🔥'));
